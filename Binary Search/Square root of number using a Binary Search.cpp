8457d9780bc8a35a877171b5aa982046faca0966
unknown <koushik.mal98@gmail.com>
2023-11-30 13:26:52 +0530

    Square root

---
+++ Binary Search/Square root of number using a Binary Search.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+  int n = 100;
+  int s=1, e=n-1, mid=s+(e-s)/2, target = 0;
+
+  while(s<=e){
+    if(mid*mid==n){
+      target=mid;
+      break;
+    }
+
+    if(mid*mid>n){
+      e=mid-1;
+    }
+
+    if(mid*mid<n){
+      // target = mid;
+      s=mid+1;
+    }
+    
+    mid = s+(e-s)/2;
+  }
+  cout<< "Square Root = "<<target<<endl;
+  
+}
